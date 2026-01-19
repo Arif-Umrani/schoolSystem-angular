da2df420ee7ad70bd90e100d1585cd61db940dac
Arif Ali <meer.aafay@gmail.com>
2026-01-19 11:34:41 +0500

    fix routing teachers add and update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,24 @@
 import { Routes } from '@angular/router';
-import { StudentList } from './modules/students/student-list/student-list';
-import { TeacherList } from './modules/teachers/teacher-list/teacher-list';
 import { Login } from './modules/auth/login/login';
+import { StudentList } from './modules/students/student-list/student-list';
 import { StudentAdd } from './modules/students/student-add/student-add';
+import { TeacherList } from './modules/teachers/teacher-list/teacher-list';
 import { TeacherAdd } from './modules/teachers/teacher-add/teacher-add';
 
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: 'students', component: StudentList,
-    children: [
-      { path: 'add', component: StudentAdd }
-    ]
-   },
-  // { path: 'students/add', component: StudentAdd },
-  { path: '', redirectTo: 'students', pathMatch: 'full' },
-  
-  { path: 'teachers/add', component: TeacherAdd},
-  
-  { path: 'teachers', component:TeacherList,
-    children: [
-      { path: 'add', component: TeacherAdd },
-    ]
-   },
 
-   { path: 'teachers/edit/:id', component: TeacherAdd},
+  //students section route
+  { path: 'students', component: StudentList },
+  { path: 'students/add', component: StudentAdd },
+  { path: 'students/edit/:id', component: StudentAdd },
+  { path: '', redirectTo: 'students', pathMatch: 'full' },
+
+
+  // teachers section route
+  { path: 'teachers', component: TeacherList },
+  { path: 'teachers/add', component: TeacherAdd },
+  { path: 'teachers/edit/:id', component: TeacherAdd },
+
 ];
