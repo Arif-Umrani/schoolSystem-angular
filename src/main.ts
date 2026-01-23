4777550613bc45ed5f301a6a30406b91ac81f6b7
Arif Ali <meer.aafay@gmail.com>
2026-01-23 19:49:09 +0500

    add bootstrap and fix studant & teacher styling

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-
+import 'bootstrap';
 
 //bootstrap import
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
